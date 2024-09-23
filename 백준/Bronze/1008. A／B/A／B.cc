0d7cdef3509631c3c6b0a1aa8a282a2d7017d46f
Seize The Day <42131095+sleepysoong@users.noreply.github.com>
2024-09-23 10:04:23 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+/*
+1008_A/B
+1116KB	0ms
+*/
+#include <cstdio>
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%.9f\n", (double)a / b);
+    return 0;
+}
