8655f4d7bb98476e6e495096115eda639653c31a
Seize The Day <42131095+sleepysoong@users.noreply.github.com>
2024-10-08 10:50:15 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    while (1) {
+        if (scanf("%d %d", &a, &b) != 2) {
+            break;
+        }
+        printf("%d\n", a + b);
+    }
+    
+    return 0;
+}
