de946af2f216afa82852304a5b813ff0ec823cb6
Seize The Day <42131095+sleepysoong@users.noreply.github.com>
2024-10-08 10:40:29 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int c, a, b;
+    scanf("%d", &c);
+    for (int i = 1; i <= c; i++) {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+    }
+    return 0;
+}
