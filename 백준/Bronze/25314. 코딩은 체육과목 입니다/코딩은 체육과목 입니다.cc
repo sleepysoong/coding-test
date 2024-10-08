3d558b35badf241a33866f09bd6ac0e9065a7ed2
Seize The Day <42131095+sleepysoong@users.noreply.github.com>
2024-10-08 10:58:19 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int byte;
+    scanf("%d", &byte);
+    for(int i = 0; i < byte / 4; i++) {
+        printf("long ");
+    }
+    printf("int");
+    return 0;
+}
