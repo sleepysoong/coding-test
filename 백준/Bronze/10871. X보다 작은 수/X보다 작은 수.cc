faff6525f231b67fefe769da788a61ade37b3bcb
Seize The Day <42131095+sleepysoong@users.noreply.github.com>
2024-10-15 10:01:52 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int n, x;
+    scanf("%d %d", &n, &x);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+        if(arr[i] < x){
+            printf("%d ", arr[i]);
+        }
+    }
+    return 0;
+}
