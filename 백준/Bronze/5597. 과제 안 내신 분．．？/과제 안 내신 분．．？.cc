592b02bd217723651fc79a2b555f9d689953fec1
Seize The Day <42131095+sleepysoong@users.noreply.github.com>
2024-10-22 03:15:48 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -1,27 +1,16 @@
 #include <stdio.h>
 
-int main(void)
-{
-    
-    /*
-    전체 학생은 30명이고 그 중 28명은 제출 완료이기에
-    제출 안한 사람은 무조건 2명이라 출력할 땐 제출 안한애를 모두 출력하면 돔
-    
-    IDEA: 0으로 초기화하고 제출한 애만 1로 하면 0인애는 과제 미제출이겠지?
-    */
-    int student[30] = {0};
-    int temp;
-
+int main(void){
+    int students[31] = {0};
     for(int i = 0; i < 28; i++){
-        scanf("%d", &temp);
-        student[temp - 1] = 1;
+        int j;
+        scanf("%d", &j);
+        students[j] = 1;
     }
-    for(int i = 0; i < 30; i++){
-        if(student[i] == 0){
-            printf("%d\n", i+1); // 인덱스는 0 ~ 29, 출석 번호는 1 ~ 30
+    for(int k = 1; k < 31; k++){
+        if(students[k] == 0){
+            printf("%d\n", k);
         }
     }
-
-
     return 0;
 }
