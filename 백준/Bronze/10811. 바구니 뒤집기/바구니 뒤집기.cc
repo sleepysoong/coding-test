c343bdafe0a210240fd2ab4cc27fa79f56dcc3f6
Seize The Day <42131095+sleepysoong@users.noreply.github.com>
2024-10-22 02:06:16 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -1,33 +1,36 @@
 #include <stdio.h>
 
-int main() {
+int main(){
+    
     int N, M;
     scanf("%d %d", &N, &M);
-
-    int baskets[101]; // 최대 100개의 바구니를 위한 배열
-    for (int i = 1; i <= N; i++) {
-        baskets[i] = i; // 바구니 초기화 (1부터 N까지)
+    
+    int basket[N+1];
+    
+    for(int i = 1; i <= N; i++){
+        basket[i] = i;
     }
-
-    for (int i = 0; i < M; i++) {
-        int L, R;
+    
+    int L, R, temp;
+    
+    for(int j = 0; j < M; j++){
         scanf("%d %d", &L, &R);
         
-        // 바구니 L부터 R까지 뒤집기
-        while (L < R) {
-            int temp = baskets[L];
-            baskets[L] = baskets[R];
-            baskets[R] = temp;
+        while(L < R){
+            temp = basket[L];
+            basket[L] = basket[R];
+            basket[R] = temp;
             L++;
             R--;
         }
+        
     }
-
-    // 결과 출력
-    for (int i = 1; i <= N; i++) {
-        printf("%d ", baskets[i]);
+    
+    for (int k = 1; k <= N; k++){
+        printf("%d ", basket[k]);
     }
-    printf("\n");
-
+    
+    
     return 0;
+    
 }
